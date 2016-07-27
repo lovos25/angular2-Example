b658007615b757774070586cfa187a44f7f58505
Amanpreet Kaur <AmanpreetK@emsnet.local>
2016-07-27 09:38:44 +0200

    exammple for loop

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,10 +3,19 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+        <h1>Addressbook</h1>
+        <ul>
+            <li *ngFor="#contact of contacts">{{contact.firstname}} {{contact.lastname}}</li>
+        </ul>
     `,
 })
-export class AppComponent {
 
+export class AppComponent {
+    contacts = [
+        {firstname: "Max", lastname: "Müller", phonenumber: "12345667", email: "max.mueller@test.de"},
+        {firstname: "Tina", lastname: "Müller", phonenumber: "12345667", email: "tine.mueller@test.de"},
+        {firstname: "Chris", lastname: "Mustermann", phonenumber: "12345667", email: "chris.mustermann@test.de"},
+        {firstname: "Caro", lastname: "Ela", phonenumber: "12345667", email: "caro.ela@test.de"},
+        {firstname: "Peet", lastname: "Hase", phonenumber: "12345667", email: "peet.hase@test.de"}
+    ];
 }
